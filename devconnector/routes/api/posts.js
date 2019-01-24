cd73a78e28f4aa20b10ef0392447fb9280cef86c
milliwonkim <39076645+milliwonkim@users.noreply.github.com>
2019-01-24 11:08:17 +0900

    Add files via upload

---
+++ devconnector/routes/api/posts.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+/**you don't need to say '/api/posts/test' 
+ * because we already did in server.js 'app.use('/api/posts', posts);'
+*/
+
+/**res.json() is similar to res.send()
+ * res.json() will automatically serve a status of 200 which means everything is OK
+*/
+router.get('/test', (req, res) => res.json({msg: "Posts Works"}));
+
+module.exports = router;
