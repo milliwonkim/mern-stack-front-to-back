08283242b516e8b59ec4e96790a97c11c8263944
milliwonkim <39076645+milliwonkim@users.noreply.github.com>
2019-02-04 16:07:05 +0900

    Add files via upload

---
+++ devconnector/client/src/reducers/index.js
@@ -0,0 +1,17 @@
+import { combineReducers } from 'redux';
+import authReducer from './authReducer';
+import errorReducer from './errorReducer';
+import profileReducer from './profileReducer';
+
+
+/**================================================================================ */
+
+export default combineReducers({
+    /**we can simply put in object with our reducers
+     * we can call whatever you want, i call it 'auth' and send it to the authorReducer
+     * so when we use anything from our authReducer in our components, will use this.props.auth
+     */
+    auth: authReducer,
+    errors: errorReducer,
+    profile: profileReducer
+});
