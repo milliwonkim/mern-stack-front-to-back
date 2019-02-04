181eacb538b30cda24f2e8e4a9ecf4ec94285cf7
milliwonkim <39076645+milliwonkim@users.noreply.github.com>
2019-02-04 16:36:40 +0900

    Update profileActions.js

--- devconnector/client/src/actions/profileActions.js
+++ devconnector/client/src/actions/profileActions.js
@@ -139,7 +139,7 @@ export const deleteEducation = id => dispatch => {
 export const getProfiles = () => dispatch => {
     dispatch(setProfileLoading());
     axios
-        .delete('/api/profile/all')
+        .get('/api/profile/all')
         .then(res => 
             dispatch({
                 type: GET_PROFILES,
