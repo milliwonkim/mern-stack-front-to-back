868455f79642386569e8d6f49a94713ec88c0901
milliwonkim <39076645+milliwonkim@users.noreply.github.com>
2019-02-07 22:59:04 +0900

    Update posts.js

--- devconnector/routes/api/posts.js
+++ devconnector/routes/api/posts.js
@@ -220,7 +220,7 @@ router.delete('/comment/:id/:comment_id',
         .then(post => {
             /**Check to see if comment exists */
             if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0){
-                return res.status(404).json({ commentnotexist: 'Comment dos not exist' });
+                return res.status(404).json({ commentnotexist: 'Comment does not exist' });
             }
             /**Get remove index */
             const removeIndex = post.comments
